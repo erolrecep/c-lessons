0eda1b40caf49537a679b6f4cefc15cbbfe2ecb7
Recep Erol <recep.90@gmail.com>
2023-06-04 22:22:25 -0500

    simpler way of linkedlist

---
+++ c_linkedlist_simple_codepal/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cc -Wall -Wextra -o main main.c -lm
+
---
+++ c_linkedlist_simple_codepal/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure of a node in the linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node with the given data
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a new node at the beginning of the linked list
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    // Create an empty linked list
+    struct Node* head = NULL;
+
+    // Insert some nodes at the beginning of the linked list
+    insertAtBeginning(&head, 5);
+    insertAtBeginning(&head, 3);
+    insertAtBeginning(&head, 1);
+
+    // Print the linked list
+    printList(head);
+
+    return 0;
+}
+
